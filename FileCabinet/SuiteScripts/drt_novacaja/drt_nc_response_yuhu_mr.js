0faf736ea5c628975003510811e23ef3aa4cdcc0
DRTIztac <58991661+DRTIztac@users.noreply.github.com>
2020-07-09 11:51:59 -0500

    Create drt_nc_response_yuhu_mr.js
    Sacript para enviar a yuhu  la  informmacion de los registros creados

---
+++ FileCabinet/SuiteScripts/drt_novacaja/drt_nc_response_yuhu_mr.js
@@ -0,0 +1,197 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType MapReduceScript
+ */
+define(['N/search', 'N/record', './drt_cn_lib', 'N/runtime', 'N/format'],
+    function (search, record, drt_cn_lib, runtime, format) {
+
+        function getInputData() {
+            try {
+                var respuesta = search.create({
+                    type: search.Type.TRANSACTION,
+                    columns: [
+                        //Campos Nativos
+                        'entity',
+                        'total',
+                        'title',
+                        'trandate',
+                        'title',
+                        //Campos Conexion por transaccion
+                        'custbody_drt_nc_con_cp',
+                        'custbody_drt_nc_con_cs',
+                        'custbody_drt_nc_con_in',
+                        'custbody_drt_nc_con_je',
+                        'custbody_drt_nc_con_so',
+                        //Campos Customm
+                        'custbody_drt_nc_notificacion_registro',
+                        'custbody_drt_nc_identificador_folio',
+                        'custbody_drt_nc_identificador_uuid',
+                        'custbody_drt_nc_folio_sustitucion',
+                        //Cammpos Opcionales
+                        'custbody_drt_nc_num_amortizacion',
+                        'custbody_drt_nc_tipo_descuento',
+                        'custbody_drt_nc_tipo_pago',
+                    ],
+                    filters: [
+                        [
+                            ['recordtype', search.Operator.IS, search.Type.INVOICE],
+                            'and',
+                            ['custbody_drt_nc_con_in', search.Operator.IS, search.Operator.ANYOF, '@NONE@']
+                        ],
+                        'or',
+                        [
+                            ['recordtype', search.Operator.IS, search.Type.CUSTOMER_PAYMMENT],
+                            'and',
+                            ['custbody_drt_nc_con_cp', search.Operator.IS, search.Operator.ANYOF, '@NONE@']
+                        ],
+                        'or',
+                        [
+                            ['recordtype', search.Operator.IS, search.Type.CASH_SALE],
+                            'and',
+                            ['custbody_drt_nc_con_cs', search.Operator.IS, search.Operator.ANYOF, '@NONE@']
+                        ],
+                        'and',
+                        ['custbody_drt_nc_notificacion_registro', search.Operator.IS, search.Operator.ISEMPTY, null]
+                    ]
+                });
+            } catch (error) {
+                log.error({
+                    title: 'error getInputData',
+                    details: JSON.stringify(error)
+                });
+            } finally {
+                return respuesta;
+            }
+
+        }
+
+        function map(context) {
+            try {
+                log.audit({
+                    title: ' context map ',
+                    details: JSON.stringify(context)
+                });
+                var objvalue = JSON.parse(context.value)
+
+                context.write({
+                    key: objvalue.id,
+                    value: objvalue
+                });
+            } catch (error) {
+                log.error({
+                    title: 'error map',
+                    details: JSON.stringify(error)
+                });
+            }
+        }
+
+        function reduce(context) {
+            try {
+                var recordData = context.values;
+                for (var ids in recordData) {
+                    try {
+                        var objupdate = {};
+                        var data = JSON.parse(recordData[ids]);
+                        log.emergency({
+                            title: 'data',
+                            details: JSON.stringify(data)
+                        });
+                        log.audit({
+                            title: 'data.values',
+                            details: JSON.stringify(data.values)
+                        });
+
+
+                        var mensajeFinal = [];
+                        switch (data.recordType) {
+                            case 'salesorder': {
+
+                            }
+                            break;
+
+                        default:
+                            mensajeFinal.push('Transaccion no valida: ' + data.recordType);
+                            break;
+                        }
+
+
+
+                    } catch (error) {
+                        log.error({
+                            title: 'error reduce',
+                            details: JSON.stringify(error)
+                        });
+                    } finally {
+                        log.audit({
+                            title: 'objupdate',
+                            details: JSON.stringify(objupdate)
+                        });
+                        if (Object.keys(objupdate).length > 0 &&
+                            data.recordType &&
+                            data.id
+                        ) {
+                            var idUpdate = record.submitFields({
+                                type: data.recordType,
+                                id: data.id,
+                                values: objupdate,
+                                options: {
+                                    enableSourcing: true,
+                                    ignoreMandatoryFields: true
+                                }
+                            });
+                        }
+                    }
+                }
+            } catch (error) {
+                log.error({
+                    title: 'error reduce',
+                    details: JSON.stringify(error)
+                });
+            }
+        }
+
+        function summarize(context) {
+            try {
+                log.audit({
+                    title: ' context summary ',
+                    details: JSON.stringify(context)
+                });
+                log.audit({
+                    title: 'Usage units consumed',
+                    details: context.usage
+                });
+                log.audit({
+                    title: 'Concurrency',
+                    details: context.concurrency
+                });
+                log.audit({
+                    title: 'Number of yields',
+                    details: context.yields
+                });
+                var text = '';
+                var totalKeysSaved = 0;
+                context.output.iterator().each(function (key, value) {
+                    text += (key + ' ' + value + '\n');
+                    totalKeysSaved++;
+                    return true;
+                });
+                log.audit({
+                    title: 'Unique number of letters used in string',
+                    details: totalKeysSaved
+                });
+            } catch (error) {
+                log.error({
+                    title: 'error summarize',
+                    details: JSON.stringify(error)
+                });
+            }
+
+        }
+
+        return {
+            getInputData: getInputData,
+            map: map,
+            reduce: reduce,
+            summarize: summarize
+        }
+    });
