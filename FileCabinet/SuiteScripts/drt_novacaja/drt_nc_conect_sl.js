fd8606f6c8addc489e2574a0fbc1e9dbcd92c889
Iztac <iztac.amaya@disruptt.mx>
2020-08-28 00:02:50 -0500

    tool
    se modifica paara pruebas unitarias

--- FileCabinet/SuiteScripts/drt_novacaja/drt_nc_conect_sl.js
+++ FileCabinet/SuiteScripts/drt_novacaja/drt_nc_conect_sl.js
@@ -47,84 +47,23 @@ define(
                 };
 
                 var form = serverWidget.createForm({
-                    title: 'DRT NC - webhook Yuhu'
+                    title: 'DRT DevelopTool'
                 });
-                var response = {
-                    data: {
-                        code: '',
-                        body: '',
-                    }
-                };
-                try {
-                    var arrayWebhook = [
-                        'create-order', //credito_inicial,
-                        'maturities-receivable', //vencimiento_por_cobrar,
-                        'update-credit', //actualiza_saldo_credito,
-                        'outstanding-balance', //saldo_pendiente_aplicar,
-                    ];
-
-                    var webhookConsultado = arrayWebhook[0];
-                    if (context.request.parameters.webhook && arrayWebhook.indexOf(context.request.parameters.webhook) >= 0) {
-                        webhookConsultado = context.request.parameters.webhook;
-                    }
-                    if (context.request.parameters.webhook && parseInt(context.request.parameters.webhook) >= 0 && parseInt(context.request.parameters.webhook) <= 3) {
-                        webhookConsultado = arrayWebhook[context.request.parameters.webhook];
-                    }
-
-                    var dataWebhook = drt_cn_lib.bookWebhook(webhookConsultado);
-                    if (dataWebhook.success) {
-                        response = drt_cn_lib.postWebhook(dataWebhook.data.header, dataWebhook.data.url, dataWebhook.data.ejemplo);
-                    } else {
-                        response.data.code = '0';
-                        response.data.body = 'No existe Webhook ';
-                    }
 
 
+                var cuentaItem = drt_cn_lib.lookup(search.Type.ITEM, 17, ['custitem_drt_accounnt_capital']);
 
-
-
-                } catch (error) {
-                    log.error({
-                        title: 'error',
-                        details: JSON.stringify(error)
-                    });
-                }
                 var objField = [{
-                        id: 'custpage_case',
-                        type: serverWidget.FieldType.TEXT,
-                        label: 'Webhook Consultado',
+                        id: 'custpage_result',
+                        type: serverWidget.FieldType.TEXTAREA,
+                        label: 'Resultado',
                         seleccion: [],
-                        defaultValue: webhookConsultado,
+                        defaultValue: JSON.stringify(cuentaItem.data.custitem_drt_accounnt_capital[0].value),
                         add: 'addField',
                         field: [],
                     },
-                    {
-                        id: 'custpage_respose',
-                        type: serverWidget.FieldType.TEXTAREA,
-                        label: 'Respuesta Yuhu Body',
-                        seleccion: [],
-                        defaultValue: JSON.stringify(response).substring(0, 4000),
-                        add: 'addField',
-                        field: [],
-                    }, {
-                        id: 'custpage_length',
-                        type: serverWidget.FieldType.TEXT,
-                        label: 'Respuesta Yuhu length',
-                        seleccion: [],
-                        defaultValue: JSON.stringify(response).length,
-                        add: 'addField',
-                        field: [],
-                    }, {
-                        id: 'custpage_code',
-                        type: serverWidget.FieldType.TEXT,
-                        label: 'Respuesta Yuhu code',
-                        seleccion: [],
-                        defaultValue: response.data.code,
-                        add: 'addField',
-                        field: [],
-                    }
+
                 ];
-                // context.response.write(JSON.stringify(response));
 
 
 
@@ -181,90 +120,8 @@ define(
             }
         }
 
-        function post_file(file_name, folder, file_type, value) {
 
-            var file_instance = file.create({
-                name: file_name,
-                fileType: file_type,
-                contents: value,
-                description: file_name,
-                folder: folder
-            });
 
-            file_instance.save();
-        }
-
-        function metadataFile(param_file_id) {
-            try {
-                var respuesta = {
-                    success: false,
-                    data: []
-                };
-                log.audit({
-                    title: 'param_file_id',
-                    details: JSON.stringify(param_file_id)
-                });
-                var objFile = file.load({
-                    id: param_file_id
-                });
-                var contenFile = objFile.getContents();
-                log.audit({
-                    title: 'contenFile',
-                    details: JSON.stringify(contenFile)
-                });
-
-                var iterator = objFile.lines.iterator();
-                log.audit({
-                    title: 'iterator',
-                    details: JSON.stringify(iterator)
-                });
-
-                iterator.each(function (line) {
-                    var lineValues = line.value.split('~');
-                    log.audit({
-                        title: 'lineValues linea 0',
-                        details: JSON.stringify(lineValues)
-                    });
-                    return false;
-                });
-
-                iterator.each(function (line) {
-                    var lineValues = line.value.split('~');
-                    log.audit({
-                        title: 'lineValues',
-                        details: JSON.stringify(lineValues)
-                    });
-                    respuesta.data.push({
-                        sublist_uuid: lineValues[0] || 0,
-                        sublist_rfc_emisor: lineValues[1] || 0,
-                        sublist_nombre_emisor: lineValues[2] || 0,
-                        sublist_rfc_receptor: lineValues[3] || 0,
-                        sublist_nombre_receptor: lineValues[4] || 0,
-                        sublist_rfc_pac: lineValues[5] || 0,
-                        sublist_fecha_emision: lineValues[6] || 0,
-                        sublist_fecha_certificacion_sat: lineValues[7] || 0,
-                        sublist_monto: lineValues[8] || 0,
-                        sublist_efecto_comprobante: lineValues[9] || 0,
-                        sublist_estatus: lineValues[10] || 0,
-                        sublist_fecha_cancelacion: lineValues[11] || 0,
-                    });
-                    return true;
-                });
-
-                respuesta.success = respuesta.data.length > 0;
-            } catch (error) {
-                log.error({
-                    title: 'error',
-                    details: JSON.stringify(error)
-                });
-            } finally {
-                log.emergency({
-                    title: 'respuesta metadataFile',
-                    details: JSON.stringify(respuesta)
-                });
-                return respuesta;
-            }
-        }
 
         return {
             onRequest: onRequest
