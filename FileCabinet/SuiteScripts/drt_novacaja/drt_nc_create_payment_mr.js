3ede2c0287b26c5c1f2696700cb8e7bdbc5a7d58
Iztac <iztac.amaya@disruptt.mx>
2020-11-30 13:14:30 -0600

    cashsale 0
    solo si se tiene parametro.custbody_drt_nc_total_interes mayor a 0 genera la cashsale de lo contrario genera entrada de diario

--- FileCabinet/SuiteScripts/drt_novacaja/drt_nc_create_payment_mr.js
+++ FileCabinet/SuiteScripts/drt_novacaja/drt_nc_create_payment_mr.js
@@ -607,7 +607,7 @@ define(['N/search', 'N/record', './drt_cn_lib', 'N/runtime', 'N/format'],
                     var loadItem = itemTransaction(record.Type.SALES_ORDER, parametro.internalid, 0);
 
                     if (
-                        parametro.custbody_drt_nc_total_interes > 0 || (parametro.item && parametro.item.length > 0)
+                        parametro.custbody_drt_nc_total_interes > 0
                     ) {
                         var objSublist_transaction = {
                             item: [],
